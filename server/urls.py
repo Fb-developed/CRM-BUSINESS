92c95a52beacc7a1b61b736c563d99a9f7193880
fayzullo <tupstadion8@gmail.com>
2025-08-26 17:53:13 +0500

    add swagger

--- server/urls.py
+++ server/urls.py
@@ -1,34 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
-
-from django.conf import settings
-from django.conf.urls.static import static
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/',include("accounts.urls")),
-    path('api/',include("api.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
+from api.urls import urlpatterns as api_urlpatterns
 
-# Swagger schema view
 schema_view = get_schema_view(
    openapi.Info(
-      title="API барои Flutter",
+      title="API Flutter",
       default_version='v1',
-      description="Ин Swagger API мебошад барои пайвастшавӣ бо Flutter",
       contact=openapi.Contact(email="example@email.com"),
    ),
    public=True,
    permission_classes=[permissions.AllowAny],
+   urlconf='api.urls'
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Шумо метавонед номи app-атро иваз кунӣ
+    path('api/', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
