ca17add6bc7bc22dc2693d3aa1e31487e067c9ec
AkmalRazokov <m46202251@gmail.com>
2025-09-05 17:12:22 +0300

    adding qrcodeserializer

--- api/serializers.py
+++ api/serializers.py
@@ -109,3 +109,48 @@ class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
         fields = '__all__'
+
+
+
+class QRCodeSaleSerializer(serializers.Serializer):
+    barcode = serializers.CharField()
+    shop_id = serializers.IntegerField()
+    payment_method_id = serializers.IntegerField()
+
+    def validate(self, data):
+        from .models import Product, Stock
+
+        try:
+            product = Product.objects.get(barcode=data['barcode'])
+        except Product.DoesNotExist:
+            raise serializers.ValidationError("Товар с таким QR-кодом не найден.")
+
+        stock = Stock.objects.filter(product=product, shop_id=data['shop_id']).first()
+        if not stock or stock.quantity < 1:
+            raise serializers.ValidationError("Недостаточно товара на складе.")
+
+        data['product'] = product
+        data['stock'] = stock
+        return data
+
+    def create(self, validated_data):
+        from .models import Transaction
+
+        product = validated_data['product']
+        stock = validated_data['stock']
+        user = self.context['request'].user
+
+        transaction = Transaction.objects.create(
+            product=product,
+            quantity=1,
+            type='out',
+            selling_price=product.price,
+            user=user,
+            payment_method_id=validated_data['payment_method_id']
+        )
+
+        stock.quantity -= 1
+        stock.save()
+
+        return transaction
+
